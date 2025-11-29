04b2875320ecd3f167098cf2f7e122502d5c67bd
Stark <448966528@qq.com>
2025-11-29 13:28:43 +0800

    feat(stark): 创建初始化sql

--- init.sql
+++ init.sql
@@ -1,11 +1,10 @@
-create table api_provider
+create table if not exists llm_provider
 (
-    id                    BIGINT(19)   not null
-        primary key,
-    provider_name         VARCHAR(100) null,
-    provider_english_name VARCHAR(50)  null,
-    service_key           VARCHAR(100) null,
-    service_url           VARCHAR(100) null,
-    create_time           DATETIME(19) null,
-    update_time           DATETIME(19) null
-);
+    id                    BIGINT   not null primary key,
+    provider_name         VARCHAR(100) null comment 'LLM提供商名称',
+    provider_english_name VARCHAR(50)  null comment 'LLM提供商英文名',
+    api_key               VARCHAR(100) null,
+    base_url              VARCHAR(100) null,
+    create_time           DATETIME null,
+    update_time           DATETIME null
+) comment 'LLM提供商表';
