1d8cd5bf9a3ae6830855452c9e2157c1b897d4bf
韦仲闯 <weizhongchuang@gwm.cn>
2025-12-12 16:22:30 +0800

    feat(Stark): 新增gpt5.2模型适配

--- service/open_router_llm.py
+++ service/open_router_llm.py
@@ -13,7 +13,9 @@ class OpenRouterLLMService(LLMService):
 
         if params['model'] in ['openai/gpt-5.2', 'openai/gpt-5.2-chat', 'openai/gpt-5.2-pro']:
             if params.get('reasoning_effort', ''):
-                params['reasoning_effort'] = 'none'
+                if params['reasoning_effort'] not in ['none', 'low', 'medium', 'high', 'xhigh']:
+                    params['reasoning'] = {'enabled': True}
+                    del params['reasoning_effort']
 
 
     async def get_usage(self, response, params, answer):
